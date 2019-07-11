23e0e4d9a50a42684c2848991462bbc928e13170
Luke Donahue <luke9389@gmail.com>
2019-07-10 23:07:02 -0700

    started project

---
+++ app.js
@@ -0,0 +1,18 @@
+// Reference needed DOM Elements
+const nounOne = document.getElementById('nounOne');
+const nounTwo = document.getElementById('nounTwo');
+const verbOne = document.getElementById('verbOne');
+
+const resultNounOne = document.getElementById('resultNounOne');
+const resultNounTwo = document.getElementById('resultNounTwo');
+const resultVerbOne = document.getElementById('resultVerbOne');
+
+// Action to take on user event
+// eslint-disable-next-line no-unused-vars
+function updateResult(){
+    resultNounOne.textContent = nounOne.value;
+    resultNounTwo.textContent = nounTwo.value;
+    resultVerbOne.textContent = verbOne.value;
+}
+
+const madlib = document.createElement
